d8eff37619ad985354d1ed0df24549bd6e002819
Andrzej <andrzejszary06@gmail.com>
2025-09-15 12:54:37 +0200

    Spadanie działa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,12 @@ public class Player : MonoBehaviour
     private Vector2 moveDelta;
     private float fixTimeMovement = 0.25f;
     private Vector2 falling;
-    private LayerMask groundLayer;
+    private GameObject[] groundLayer;
     private BoxCollider2D collider;
 
     void Start()
     {
-        groundLayer = LayerMask.GetMask("Ground");
+        groundLayer = GameObject.FindGameObjectsWithTag("Ground");
     }
 
     private void FixedUpdate()
@@ -18,15 +18,24 @@ public class Player : MonoBehaviour
         moveDelta = Vector2.zero;
         falling = new Vector2(0, -1);
 
-        transform.Translate(falling);
-
+        for (int i = 0; i < groundLayer.Length; i++)
+        {
+            Debug.Log(transform.position + " " + groundLayer[i].transform.position);
+            if (transform.position.Equals(groundLayer[i].transform.position))
+            {
+                Debug.Log("Dzia³a!");
+                falling = Vector2.zero;
+                break;
+            }
+        }
 
         float x = Input.GetAxisRaw("Horizontal");
-
         moveDelta = new Vector2(x, 0);
-
         transform.Translate(moveDelta * fixTimeMovement);
+    }
 
-
+    private void LateUpdate()
+    {
+        transform.Translate(falling * fixTimeMovement);
     }
 }
