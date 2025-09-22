16e0e42829de8dc396837f5975712554f39ecee9
Andrzej <andrzejszary06@gmail.com>
2025-09-22 15:45:04 +0200

    Dodano chesta

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,23 @@ public class Player : MonoBehaviour
 {
     private Vector2 moveDelta;
     private float fixTimeMovement = 0.25f;
-    private Vector2 falling;
-    private GameObject[] groundLayer;
+    [SerializeField] private float jumpHight = 25;
+    private Rigidbody2D rb;
 
-    void Start()
+    private void Start()
     {
-        groundLayer = GameObject.FindGameObjectsWithTag("Ground");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {     
-        moveDelta = Vector2.zero;
-        falling = new Vector2(0, -1);
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
-
-        if (hit)
-        {
-            float distance = Mathf.Abs(hit.point.y - transform.position.y);
-            Debug.Log(distance);
-        }
-
         float x = Input.GetAxisRaw("Horizontal");
         moveDelta = new Vector2(x, 0);
         transform.Translate(moveDelta * fixTimeMovement);
-    }
 
-    private void LateUpdate()
-    {
-        transform.Translate(falling * fixTimeMovement);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            rb.linearVelocity = new Vector2(0, jumpHight);
+        }
     }
 }
