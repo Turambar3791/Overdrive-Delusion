e022510ab0fe0700eb21f3f45143e73bc992d6d6
Andrzej <andrzejszary06@gmail.com>
2025-09-24 16:30:06 +0200

    groundcheck

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,15 @@ public class Player : MonoBehaviour
 {
     private Vector2 moveDelta;
     private float fixTimeMovement = 0.25f;
-    [SerializeField] private float jumpHight = 25;
+    private float jumpHight = 25;
     private Rigidbody2D rb;
+    [SerializeField] Transform groundCheck;
+    private BoxCollider2D groundCheckColl;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        groundCheckColl = groundCheck.GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
@@ -18,9 +21,17 @@ public class Player : MonoBehaviour
         moveDelta = new Vector2(x, 0);
         transform.Translate(moveDelta * fixTimeMovement);
 
+        Debug.Log(groundCheckColl.IsTouchingLayers(LayerMask.GetMask("Ground")));
         if (Input.GetKeyDown(KeyCode.C))
         {
-            rb.linearVelocity = new Vector2(0, jumpHight);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHight);
         }
+
+    }
+
+    private bool IsGrounded()
+    {
+        var test = groundCheckColl.gameObject.CompareTag("Ground");
+        return test;
     }
 }
