cc4a8921e20ff03c02b46a110b4e7f50963c8758
Andrzej <andrzejszary06@gmail.com>
2025-09-29 15:14:56 +0200

    Zeslizginowanie sie ze scian

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,24 +7,47 @@ public class Player : MonoBehaviour
     private float jumpHight = 25;
     private Rigidbody2D rb;
     [SerializeField] Transform groundCheck;
+    [SerializeField] Transform wallCheckLeft;
+    [SerializeField] Transform wallCheckRight;
     private BoxCollider2D groundCheckColl;
+    private BoxCollider2D wallCheckLeftColl;
+    private BoxCollider2D wallCheckRightColl;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         groundCheckColl = groundCheck.GetComponent<BoxCollider2D>();
+        wallCheckLeftColl = wallCheckLeft.GetComponent<BoxCollider2D>();
+        wallCheckRightColl = wallCheckRight.GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
     {     
+        // poruszanie siê prawo-lewo
         float x = Input.GetAxisRaw("Horizontal");
+        // cancelowanie ruchu w œcianê podczas wallJumpa
+        if ((x < 0 && IsTouchingWallOnTheLeft()) || (x > 0 && IsTouchingWallOnTheRight()))
+        {
+            x = 0;
+        }
         moveDelta = new Vector2(x, 0);
         transform.Translate(moveDelta * fixTimeMovement);
-
-        Debug.Log(IsGrounded());
-        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
+        
+        // skakanie
+        if (Input.GetKeyDown(KeyCode.C) && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHight);
+        }
+
+        // zeœlizgiwanie siê ze œcian
+        if (IsTouchingWallOnTheLeft() || IsTouchingWallOnTheRight())
+        {
+            rb.gravityScale = 6;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        }
+        else
+        {
+            rb.gravityScale = 10;
         }
 
     }
@@ -32,5 +55,15 @@ public class Player : MonoBehaviour
     private bool IsGrounded()
     { 
         return groundCheckColl.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+
+    private bool IsTouchingWallOnTheLeft()
+    { 
+        return wallCheckLeftColl.IsTouchingLayers(LayerMask.GetMask("Wall"));
+    }
+
+    private bool IsTouchingWallOnTheRight()
+    {
+        return wallCheckRightColl.IsTouchingLayers(LayerMask.GetMask("Wall"));
     }
 }
