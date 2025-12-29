c4a6a57698f82bdf480d52b52734a485244e0085
Turambar3791 <andrzejszary06@gmail.com>
2025-12-29 23:43:05 +0100

    yay

--- Assets/Scripts/Autosave.cs
+++ Assets/Scripts/Autosave.cs
@@ -20,7 +20,7 @@ public class Autosave : MonoBehaviour
 
     IEnumerator Save()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         if (SaveLoadSystem.Instance.data.is3rdLevelCompleted)
         {
             SceneManager.LoadScene("TheEnd");
